8f33fbf3fa2b2b99d16eaf28974301e7764adecb
Nur Alam <104093038+nurnfs2@users.noreply.github.com>
2023-07-03 22:48:11 +0600

    Update ostad_db_laravel.sql

--- ostad_db_laravel.sql
+++ ostad_db_laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 04, 2023 at 07:26 PM
+-- Generation Time: Jul 03, 2023 at 04:43 PM
 -- Server version: 5.7.36
 -- PHP Version: 8.1.0
 
@@ -18,8 +18,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ostad_db_laravel`
+-- Database: `ostad_db_laravel18`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Electronics', '2023-07-03 15:48:05', '2023-07-03 15:48:05'),
+(2, 'Foods', '2023-07-03 15:48:05', '2023-07-03 15:48:05'),
+(3, 'Medicince', '2023-07-03 15:48:05', '2023-07-03 15:48:05'),
+(4, 'Vehicle', '2023-07-03 15:48:05', '2023-07-03 15:48:05');
 
 -- --------------------------------------------------------
 
@@ -52,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -63,26 +88,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_06_04_185211_create_products_table', 1),
-(7, '2023_06_04_190340_add_category_to_products_table', 3),
-(8, '2023_06_04_190635_create_orders_table', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `product_id` bigint(20) UNSIGNED NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `orders_product_id_foreign` (`product_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+(5, '2023_07_03_141828_create_categories_table', 1),
+(6, '2023_07_03_142233_create_posts_table', 2),
+(7, '2023_07_03_142513_add_category_id_to_posts_table', 3),
+(8, '2023_07_03_142713_create_products_table', 3),
+(9, '2023_07_03_155623_add_deleted_at_to_posts_table', 4);
 
 -- --------------------------------------------------------
 
@@ -124,6 +134,36 @@ CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `category_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `posts_category_id_foreign` (`category_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `created_at`, `updated_at`, `category_id`, `deleted_at`) VALUES
+(1, 'Test Data', 'Test Data', '2023-07-03 15:49:07', '2023-07-03 15:49:07', 1, NULL),
+(2, 'Test Data 2', 'Test Data 2', '2023-07-03 15:49:07', '2023-07-03 15:49:07', 2, NULL),
+(3, 'Medicine Test Data', 'Medicine Test Data', '2023-07-03 15:49:07', '2023-07-03 15:49:07', 3, NULL),
+(4, 'Medicine Test Data 2', 'Medicine Test Data 2', '2023-07-03 15:49:07', '2023-07-03 15:49:07', 3, NULL),
+(5, 'Medicine Test Data 3', 'Medicine Test Data 3', '2023-07-03 15:49:07', '2023-07-03 15:49:07', 3, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -131,14 +171,23 @@ DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `category` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `price` decimal(8,2) NOT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `price` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `category_id` bigint(20) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `products_category_id_foreign` (`category_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`, `category_id`, `created_at`, `updated_at`) VALUES
+(1, 'Napa', '10', 'napa.jpg', 3, '2023-07-03 15:50:26', '2023-07-03 15:50:26'),
+(2, 'Napa Extra', '12', 'napa.jpg', 3, '2023-07-03 15:50:26', '2023-07-03 15:50:26'),
+(3, 'Napa Extra plus', '12', 'napa.jpg', 3, '2023-07-03 15:50:26', '2023-07-03 15:50:26');
 
 -- --------------------------------------------------------
 
